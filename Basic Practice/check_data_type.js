f055b7fc8f8f2a106259bcea99367f2a156498cc
mita07 <mymsar@0311>
2016-12-20 17:12:33 +0600

    Check data type using "isNaN"

--- Basic Practice/check_data_type.js
+++ Basic Practice/check_data_type.js
@@ -1,11 +1,11 @@
 function checkDataType(variable) {
     var value;
     if(value = parseFloat(variable)){
-        console.log(value);
+        console.log("Result is = " + value);
     }
     else if(value = isNaN(variable)){
-        console.log("Result is 0 ");
+        console.log("Result is = 0 ");
 
     }
 }
-checkDataType("ABCD");
+checkDataType("asdd");
